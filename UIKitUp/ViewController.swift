9204386c3f5dacbf8f362609964d8f1155d4e02f
Ivan <saitez377@gmail.com>
2022-08-30 12:27:38 +0300

    working out UIKit

--- UIKitUp/ViewController.swift
+++ UIKitUp/ViewController.swift
@@ -9,11 +9,86 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainLabel: UILabel!
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var button: UIButton!
+    @IBOutlet var datePicker: UIDatePicker!
+    @IBOutlet var switchForm: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // MARK: segmented Control
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: false)
+        
+        // MARK: Label
+        
+        mainLabel.text = ""
+        mainLabel.font = mainLabel.font.withSize(30)
+        mainLabel.textAlignment = .center
+        mainLabel.numberOfLines = 2
+        
+        // MARK: Slider
+        slider.value = 1
+        slider.minimumValue = 1
+        slider.maximumValue = 100
+        slider.minimumTrackTintColor = .yellow
+        slider.maximumTrackTintColor = .blue
+        slider.thumbTintColor = UIColor(red: 255/255, green: 0/255, blue: 0/255, alpha: 1)
+        
+        mainLabel.text = String(slider.value)
+        
+        
+        // MARK: Text Field
+        textField.backgroundColor = .white
+        
+        // MARK: Button
+        button.layer.cornerRadius = 10
+        button.setTitle("Готово", for: .normal)
+        
+        // MARK: Date Picker
     }
 
-
+    @IBAction func segmentedControlAction(_ sender: Any) {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            mainLabel.text = "Выбран сегмент с индексом 0"
+            mainLabel.textColor = .red
+        case 1:
+            mainLabel.text = "Выбран сегмент с индексом 1"
+            mainLabel.textColor = .yellow
+        case 2:
+            mainLabel.text = "Выбран сегмент с индексом 2"
+            mainLabel.textColor = .green
+        default: break
+        }
+    }
+    @IBAction func sliderAction() {
+        mainLabel.text = String(Int(slider.value))
+    }
+    @IBAction func buttonAction(_ sender: Any) {
+        guard let text = textField.text, !text.isEmpty else { return }
+            mainLabel.text = text
+        
+    }
+    @IBAction func datePickerAction(_ sender: Any) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .full
+        
+        mainLabel.text = dateFormatter.string(from: datePicker.date)
+    }
+    
+    @IBAction func switchAction(_ sender: Any) {
+        segmentedControl.isHidden = !switchForm.isOn
+        mainLabel.isHidden = !switchForm.isOn
+        slider.isHidden = !switchForm.isOn
+        textField.isHidden = !switchForm.isOn
+        button.isHidden = !switchForm.isOn
+        datePicker.isHidden = !switchForm.isOn
+        
+    }
+    
 }
 
